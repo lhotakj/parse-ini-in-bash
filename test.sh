6dc8c28fc17051485d7c8f05b094b4b6265f29f7
lhotakj <lhotakj@gmail.com>
2020-02-15 10:43:42 +0100

    passing filename to functions

--- test.sh
+++ test.sh
@@ -3,8 +3,9 @@
 INI_FILE="./demo.ini"
 
 # returns an array of all sections
+# $1 - file
 function get_sections_array() {
-  ARRAY=($(egrep '\[.*\]' ${INI_FILE}))
+  ARRAY=($(egrep '\[.*\]' $1))
   FIXED_ARRAY=""
   for i in "${ARRAY[@]}"; do
     FIX=$i
@@ -17,25 +18,21 @@ function get_sections_array() {
 }
 
 # returns value of specific key in given section
-# $1 - section
+# $1 - file
 # $2 - key
+# $3 - section
 function get_value_from_section_key() {
-  echo "$(sed -nr "/^\[$1\]/ { :l /^$2[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ${INI_FILE})"
+  echo "$(sed -nr "/^\[$2\]/ { :l /^$3[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $1)"
 }
 
-echo "== demo =="
-echo "file: ${INI_FILE}"
-echo "--- list of sections (begin) ---"
-# convert to
-declare -a TARGETS="$(get_sections_array)"
+echo "== get_sections_array =="
+# convert to array
+declare -a TARGETS="$(get_sections_array ${INI_FILE})"
 for obj in ${TARGETS[@]}; do
     echo "$obj"
 done
-echo "--- list of sections (end) ---"
 
-declare VAL=$(get_value_from_section_key "section1" "path")
-echo "value of key 'path' in section 'section1': $VAL"
-declare VAL=$(get_value_from_section_key "section2" "params")
-echo "value of key 'params' in section 'section2': $VAL"
-declare VAL=$(get_value_from_section_key "section3" "path")
-echo "value of key 'path' in section 'section3': $VAL"
+echo "== get_value_from_section_key =="
+echo "value of key 'path' in section 'section1':   $(get_value_from_section_key ${INI_FILE} "section1" "path")"
+echo "value of key 'params' in section 'section2': $(get_value_from_section_key ${INI_FILE} "section2" "params")"
+echo "value of key 'path' in section 'section3':   $(get_value_from_section_key ${INI_FILE} "section3" "path")"
